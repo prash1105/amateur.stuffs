7c2cf4d21d0ecfcc4770cc494ef37ec9e49f6503
Prashant <64917832+prash1105@users.noreply.github.com>
2021-02-09 12:22:03 +0530

    Add files via upload

---
+++ MC-1.2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double n1, n2, prod;
+    cout << "Enter your first number: ";
+    cin >> n1;
+    
+    cout << "Enter your second number: ";
+    cin >> n2;
+ 
+    prod = n1 * n2;  
+
+    cout << "Product = " << prod;    
+    
+    return 0;
+}
